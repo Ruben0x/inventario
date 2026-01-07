62860770000b0ad6bb4b7a4ebc63ef9023d0b411
Ruben0x <r.alejandro.gn@gmail.com>
2026-01-06 21:40:28 -0300

    feat: :sparkles: added new product3

--- app/src/main/java/inventario/App.java
+++ app/src/main/java/inventario/App.java
@@ -9,11 +9,13 @@ public class App {
 
         Producto producto1 = new Producto("2", "Producto 1", 10.5, 5);
         Producto producto2 = new Producto("3", "Producto 2", 20.0, 3);
+        Producto producto3 = new Producto("4", "Producto 3", 23.0, 3);
 
         producto1.mostrarProducto();
 
         String nombreProducto = producto2.getNombre();
 
         System.out.println("Nombre del producto 2: " + nombreProducto);
+        System.out.println("Nombre del producto 3: " + producto3.getNombre());
     }
 }
