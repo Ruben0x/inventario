a20de5609163456eeaa40ccd72fa7446a13f38f6
Ruben0x <r.alejandro.gn@gmail.com>
2026-01-06 21:45:33 -0300

    refactor: :fire: remove product2 and product3

--- app/src/main/java/inventario/App.java
+++ app/src/main/java/inventario/App.java
@@ -8,11 +8,8 @@ public class App {
     public static void main(String[] args) {
 
         Producto producto1 = new Producto("2", "Producto 1", 10.5, 5);
-        Producto producto2 = new Producto("3", "Producto 2", 20.0, 3);
-        Producto producto3 = new Producto("4", "Producto 3", 23.0, 3);
 
         producto1.mostrarProducto();
-        producto2.mostrarProducto();
-        producto3.mostrarProducto();
+
     }
 }
