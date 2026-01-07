3b242745dd8a4f93085aaba3c904e4cd63f6aae3
Ruben0x <r.alejandro.gn@gmail.com>
2026-01-06 21:38:54 -0300

    fix: :ambulance: product2 name

--- app/src/main/java/inventario/App.java
+++ app/src/main/java/inventario/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) {
 
         Producto producto1 = new Producto("2", "Producto 1", 10.5, 5);
-        Producto producto2 = new Producto("3", "Producto 3", 20.0, 3);
+        Producto producto2 = new Producto("3", "Producto 2", 20.0, 3);
 
         producto1.mostrarProducto();
 
